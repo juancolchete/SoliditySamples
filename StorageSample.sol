7f6c55b9e23b8033f26604131dbb9bf1a1179d1c
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-10-24 12:00:39 -0300

    Fix tipo Retrieve function

--- StorageSample.sol
+++ StorageSample.sol
@@ -20,7 +20,7 @@ contract SimpleStorage{
         favoriteNumber = _favoriteNumber;
     }
     
-    function retrive() public view returns(uint256){
+    function retrieve() public view returns(uint256){
         return favoriteNumber;
     }
     
