a4dbf215d11de378cc16252bbb34116509cde788
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2022-08-14 09:12:32 -0300

    Add returnGivenAddress

--- sandbox/testDefaultData.sol
+++ sandbox/testDefaultData.sol
@@ -5,4 +5,7 @@ contract defaultData{
     function reurnDefaultAddress() public pure returns (address) {
         return address(0);
     }
+    function returnGivenAddress(address receiver) public pure returns(address){
+        return receiver;
+    }
 }
